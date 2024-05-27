8f87bf86b9209a6cf02904782f6cba919e25ae52
Monaheng57662279 <117026971+Monaheng57662279@users.noreply.github.com>
2024-05-27 14:59:24 +0200

    Add files via upload

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from myapp import views
 from myapp.views import upload_csv, predict_fraud_single, dashboard, predict_fraud_multiple
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +14,12 @@ urlpatterns = [
     path('myapp/single_file/', views.predict_fraud_single, name='single_file'),
     path('myapp/multiple_file/', views.predict_fraud_multiple, name='multiple_file'),
     path('myapp/Dashboard/', views.dashboard, name='dashboard'),
+    path('myapp/login/', views.login_view, name='login'),
+    path('myapp/logout/', views.logout_view, name='logout'),
+    path('myapp/home/', views.home, name = 'home'),
+    path('myapp/create/', views.create_transaction, name='create_transaction'),
+    path('myapp/reports/', views.reports, name='reports'),
+    # path('upload_data/',views.upload_data,name='upload_data'),
+    path('myapp/delete_record/', views.delete_record, name='delete_record'),
+    path('myapp/about/', views.about_admin_dashboard, name='about'),
 ]
